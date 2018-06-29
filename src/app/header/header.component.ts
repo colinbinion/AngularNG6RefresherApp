cac2985fa00ff7b3e2e0e31ee14db4c5b464ad7b
Colin Binion <colin.binion@me.com>
2018-06-29 16:58:41 -0600

    header component manually added and tested

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+//added manually bc it will be used exclusively by app component
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+
+export class HeaderComponent {
+
+}
